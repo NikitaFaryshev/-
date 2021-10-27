7029b456b82df7bf7540e05c218471d3d45c97ae
NikitaFaryshev <tibanedggg@yandex.ru>
2021-10-27 20:20:01 +0400

    Создан новый проект

---
+++ JFDH/JFDH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JFDH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
