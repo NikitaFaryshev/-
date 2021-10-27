c621ba8d851c48f676d74e3723f19f6d3a514846
NikitaFaryshev <tibanedggg@yandex.ru>
2021-10-27 20:24:36 +0400

    Изменение

--- JFDH/JFDH/Program.cs
+++ JFDH/JFDH/Program.cs
@@ -6,7 +6,7 @@ namespace JFDH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Алла Пугачёва лучшая вообще класс супер!");
         }
     }
 }
